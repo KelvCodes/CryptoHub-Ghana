59a14528f7620308766a87a950a3c0b3d5a85e9a
Kelvin Agyare <onlykelvin06@gmail.com>
2025-07-05 04:17:56 -0700

    Update NegativeNumbers.sol

--- fixtures/contracts/NegativeNumbers.sol
+++ fixtures/contracts/NegativeNumbers.sol
@@ -1,4 +1,4 @@
-erIndexed(int256 indexed one);
+
 	event TwoNegativeNumbers(int256 one, int256 two);
 	event TwoNegativeNumbersIndexed(int256 indexed one, int256 indexed two);
 	event OtherNegativeNumbers(uint256 positive, int256 negative, string str);
