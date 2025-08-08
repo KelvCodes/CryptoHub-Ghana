4a9bdfc62e1f08513eddf0fed26122b8c53d0b6b
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-08 11:36:34 -0700

    Update ERC721Token.sol

--- fixtures/contracts/ERC721Token.sol
+++ fixtures/contracts/ERC721Token.sol
@@ -1,6 +1,5 @@
 
-
-pragma solidity ^0.8.13;
+a solidity ^0.8.13;
 
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
