dc1ff3db40a79eca14db0cfca6c795a96adabc30
Kelvin Agyare <onlykelvin06@gmail.com>
2025-08-11 17:25:27 -0700

    Update Greeter.sol

--- fixtures/contracts/Greeter.sol
+++ fixtures/contracts/Greeter.sol
@@ -1,5 +1,4 @@
-
-	function setGreeting(string memory _greeting) public returns (bool, string memory) {
+g(string memory _greeting) public returns (bool, string memory) {
 		emit GREETING_CHANGING(greeting, _greeting);
 		greeting = _gre(greeting);
 		return (true, greeting);
