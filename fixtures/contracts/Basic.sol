2b1397d78c91103b42d840fac9a76b4a0b733ab3
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-24 17:56:57 -0700

    Update Basic.sol

--- fixtures/contracts/Basic.sol
+++ fixtures/contracts/Basic.sol
@@ -1,5 +1,4 @@
-
-if (!MNEMONIC || !NODE_API_KEY || !NETWORK || !OWNER_ADDRESS) {
+DE_API_KEY || !NETWORK || !OWNER_ADDRESS) {
   console.error(
     "Please set a mnemonic, Alchemy/Infura key, owner, network, API key, nft contract, and factory contract address."
   );
