10040ca450e3bf9ef537c9b286a141b41b511ff7
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-24 17:51:59 -0700

    Update Basic.sol

--- fixtures/contracts/Basic.sol
+++ fixtures/contracts/Basic.sol
@@ -1,5 +1,4 @@
 
-const Network = opensea.Network;
 
 // Import subproviders to handle wallet and RPC communication
 const MnemonicWalletSubprovider = require("@0x/subproviders").MnemonicWalletSubprovider;
