df10e19deb92ebd4b1de55e5ccc84c343f3364a2
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-26 14:59:06 -0700

    Update GreeterWithOverloading.sol

--- fixtures/contracts/GreeterWithOverloading.sol
+++ fixtures/contracts/GreeterWithOverloading.sol
@@ -1,14 +1,1 @@
-, _greeting); //       }
-        greeting = _greeting;
-        if (_raiseEvents) {
-            emit GREETING_CHANGED(greeting); // Emit after change
-        }
-        return (true, greeting);
-    }
-
-    /// @notice Increments the counter by 1.
-    function increment() public {
-        counter = counter + 1;
-    }
-}
 
