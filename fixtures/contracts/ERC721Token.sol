2885c849add5da7b6788e3c9d520aacfb3ff5ea5
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-27 15:21:53 -0700

    Update ERC721Token.sol

--- fixtures/contracts/ERC721Token.sol
+++ fixtures/contracts/ERC721Token.sol
@@ -1,5 +1,4 @@
-
-     * @dev Increments the token ID counter, mints tn the new token
+ counter, mints tn the new token
      * @param tokenURI The metadata URI associated with the token (e.g., JSON metadata link)
      * @return The ID of the newly minted token
      */
