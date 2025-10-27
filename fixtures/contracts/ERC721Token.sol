45464c2c96f8ea2e32663c98049274cc21433164
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-27 15:22:18 -0700

    Update ERC721Token.sol

--- fixtures/contracts/ERC721Token.sol
+++ fixtures/contracts/ERC721Token.sol
@@ -1,4 +1,4 @@
-URI n awardItem(address player, string memory tokenURI) public returns (uint256) {
+awardItem(address player, string memory tokenURI) public returns (uint256) {
         // Get the current token ID from the counter
         uint256 newItemId = _tokenIds.current();
 
