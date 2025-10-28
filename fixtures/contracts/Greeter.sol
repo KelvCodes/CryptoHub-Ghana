839bea44db1986752262ba1aab9a65d53f59ace9
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-28 14:08:49 -0700

    Update Greeter.sol

--- fixtures/contracts/Greeter.sol
+++ fixtures/contracts/Greeter.sol
@@ -1,4 +1,4 @@
-eading/updating the greeting, keeps track of changes via events,
+dating the greeting, keeps track of changes via events,
 // and includes a basic counter that can be incremented.
 contract Greeter {
     // State variable to store a numerical counter
