0f418c8c93aa6021d8a6ae81fc2280d0e511a13c
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-27 23:31:20 -0700

    Update ERC721Token.sol

--- fixtures/contracts/ERC721Token.sol
+++ fixtures/contracts/ERC721Token.sol
@@ -1,6 +1,6 @@
 
 
-        // Increment the token ID counter for the next mint
+     Increment the token ID counter for the next mint
         _tokenIds.increment();
 
         // Return the ID of the newly minted token
