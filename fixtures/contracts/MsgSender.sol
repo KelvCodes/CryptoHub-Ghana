8cbb2ec4d245f9e8ccb8fc0b5d4eb2cd5d62f0bc
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-29 15:10:00 -0700

    Update MsgSender.sol

--- fixtures/contracts/MsgSender.sol
+++ fixtures/contracts/MsgSender.sol
@@ -1,5 +1,4 @@
 
-    // Takes a string input _testString, updates the stored testString,
     // and returns a boolean indicating success and the new string value.
     function setTestString(string memory _testString) public returns (bool, string memory) {
         testString = _testString;
