273a0b6b923471b330f522de8a7b1d200da468c6
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-29 15:06:33 -0700

    Update MsgSender.sol

--- fixtures/contracts/MsgSender.sol
+++ fixtures/contracts/MsgSender.sol
@@ -1,6 +1,5 @@
 
-
-    // Public view function that returns the address of the message sender.
+c view function that returns the address of the message sender.
     // This is the address that called the current function.
     function from() public view returns (address) {
         return msg.sender;
