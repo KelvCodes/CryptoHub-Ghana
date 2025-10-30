74502f9d41fc7c78fb200e33092c369f07a7eb6a
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-30 14:18:09 -0700

    Update ERC721Token.sol

--- fixtures/contracts/ERC721Token.sol
+++ fixtures/contracts/ERC721Token.sol
@@ -1,15 +1,1 @@
 
-     * @dev Allows the contract owner to update the base metadata URI
-     */
-    function setBaseURI(string memory newBaseURI) public onlyOwner {
-        _baseTokenURI = newBaseURI;
-    }
-
-    /**
-     * @dev Override _baseURI() to return the dynamic base URI
-     */
-    function _baseURI() internal view override returns (string memory) {
-        return _baseTokenURI;
-    }
-}
-
