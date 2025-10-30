ce68ec5370e328bf8e82b35cd7141ea8f314809a
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-30 14:18:04 -0700

    Update ERC721Token.sol

--- fixtures/contracts/ERC721Token.sol
+++ fixtures/contracts/ERC721Token.sol
@@ -1,15 +1,4 @@
 
-   ;
-     * @dev Allows a token owner to burn their NFT
-  
-    /**
-     * @dev Returns the total number of tokens minted so far
-     */
-    function totalSupply() public view returns (uint256) {
-        return _tokenIds.current();
-    }
-
-    /**
      * @dev Allows the contract owner to update the base metadata URI
      */
     function setBaseURI(string memory newBaseURI) public onlyOwner {
