1067837b1f4749ab096a5694c92600144158423c
Kelvin Agyare <onlykelvin06@gmail.com>
2025-10-31 18:46:04 -0700

    Update ERC721Token.sol

--- fixtures/contracts/ERC721Token.sol
+++ fixtures/contracts/ERC721Token.sol
@@ -1,8 +1,6 @@
 
 
-// ============================================================
-// Zeppelin Libraries
-// ============================================================
+// ===============================================
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
