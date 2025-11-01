c30f4b58a8dd50880a2addee2a3020b732265eb6
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-01 07:12:30 -0700

    Update Greeter.sol

--- fixtures/contracts/Greeter.sol
+++ fixtures/contracts/Greeter.sol
@@ -1,5 +1,4 @@
-eGreeting(string memory _newGreeting) {
-        require(
+
             keccak256(bytes(_newGreeting)) != keccak256(bytes(greeting)),
             "New greeting must be different from the current one"
         );
