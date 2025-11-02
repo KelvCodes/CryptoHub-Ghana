81ce4b8a931424347881733d917f5a58899cbce1
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-02 00:03:44 -0700

    Update ErrorsContract.sol

--- fixtures/contracts/ErrorsContract.sol
+++ fixtures/contracts/ErrorsContract.sol
@@ -1,4 +1,4 @@
- is authorized
+
     /// @dev Reverts with a custom `Unauthorized` error if the caller is not the owner
     function unauthorize() public {
         // Check if the function caller is NOT the owner
