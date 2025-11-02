d0ac22abaa7ebec6914553caf48c775d16a69262
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-02 00:00:11 -0700

    Update ErrorsContract.sol

--- fixtures/contracts/ErrorsContract.sol
+++ fixtures/contracts/ErrorsContract.sol
@@ -1,4 +1,4 @@
-onstrates usage of custom errors and revert handling
+ge of custom errors and revert handling
 contract ErrorsContract {
     // Declare the owner address as payable (so it can receive Ether)
     address payable owner = payable(0x0);
