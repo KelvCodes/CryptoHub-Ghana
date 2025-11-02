accf52b51787c81b4a5dd2e82f396372f41bd55f
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-02 00:02:12 -0700

    Update ErrorsContract.sol

--- fixtures/contracts/ErrorsContract.sol
+++ fixtures/contracts/ErrorsContract.sol
@@ -1,4 +1,4 @@
-notice Constructor sets the owner to a default zero address (can be customized)
+ Constructor sets the owner to a default zero address (can be customized)
     constructor() {}
 
     /// @notice Attempts to transfer the contract balance to the owner if caller is authorized
