35ea8fbe1baab35a19147f93fcdecad6c33b4dd5
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-02 00:02:44 -0700

    Update ErrorsContract.sol

--- fixtures/contracts/ErrorsContract.sol
+++ fixtures/contracts/ErrorsContract.sol
@@ -1,4 +1,4 @@
-to a default zero address (can be customized)
+ult zero address (can be customized)
     constructor() {}
 
     /// @notice Attempts to transfer the contract balance to the owner if caller is authorized
