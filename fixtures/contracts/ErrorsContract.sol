378ca7778c44b09bb1196de2dd2dce2af01950c5
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-02 00:06:54 -0700

    Update ErrorsContract.sol

--- fixtures/contracts/ErrorsContract.sol
+++ fixtures/contracts/ErrorsContract.sol
@@ -1,5 +1,5 @@
-enction to demonstrate the use of a custom error with a string
-    /// @dev This always reverts using a custom error `CustomError`
+ always reverts using a custom error `CustomError`
+
     function badRequire() public {
         // This condition is always true (1 < 2), so the revert always triggers
         if (1 < 2) revert CustomError("reverted using custom Error");
