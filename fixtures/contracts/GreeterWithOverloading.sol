10cfb147ab515f0feadcd31dab373190ace07c84
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-02 22:41:59 -0800

    Update GreeterWithOverloading.sol

--- fixtures/contracts/GreeterWithOverloading.sol
+++ fixtures/contracts/GreeterWithOverloading.sol
@@ -1,4 +1,4 @@
- - An enhanced smart contract demonstrating function overloading,
+smart contract demonstrating function overloading,
 /// event emissions, state management, and basic access control.
 /// @author 
 /// Kelvin A.
