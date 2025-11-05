bc4a9ec4cd134564856fcbff652af18f6f992a9d
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-05 15:28:13 -0800

    Update MyContract.sol

--- fixtures/contracts/MyContract.sol
+++ fixtures/contracts/MyContract.sol
@@ -1,4 +1,4 @@
-version required to compile this contract
+ion required to compile this contract
 // Version 0.8.13 or higher (but below 0.9.0) is compatible
 
 // Define the contract named 'MyContract'
