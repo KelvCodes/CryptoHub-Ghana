eb945e68bd58099ab39700623a277f4595368ad8
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-05 15:27:41 -0800

    Update MyContract.sol

--- fixtures/contracts/MyContract.sol
+++ fixtures/contracts/MyContract.sol
@@ -1,5 +1,4 @@
-
-// Declares the Solidity compiler version required to compile this contract
+es the Solidity compiler version required to compile this contract
 // Version 0.8.13 or higher (but below 0.9.0) is compatible
 
 // Define the contract named 'MyContract'
