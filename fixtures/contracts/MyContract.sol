c364984e297f38611b26dd17478e87ed63c87dc7
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-05 15:36:57 -0800

    Update MyContract.sol

--- fixtures/contracts/MyContract.sol
+++ fixtures/contracts/MyContract.sol
@@ -1,4 +1,4 @@
-ate myAttribute;
+myAttribute;
 
     // Public function to retrieve the value of 'myAttribute'
     // The 'view' keyword indicates that this function does not modify the blockchain state
