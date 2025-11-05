bff6210c55f0d5acb543de6c9d777a284cc8c2b2
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-05 15:37:40 -0800

    Update MyContract.sol

--- fixtures/contracts/MyContract.sol
+++ fixtures/contracts/MyContract.sol
@@ -1,4 +1,4 @@
-on to retrieve the value of 'myAttribute'
+rieve the value of 'myAttribute'
     // The 'view' keyword indicates that this function does not modify the blockchain state
     // Returns: The stored string value of 'myAttribute'
     function getAttr() public view returns (string memory) {
