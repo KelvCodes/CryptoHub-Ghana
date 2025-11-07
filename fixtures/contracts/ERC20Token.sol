f2b98f6d1cd5ab2ec6442c54f475d2915627c0e6
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-07 01:33:46 -0800

    Update ERC20Token.sol

--- fixtures/contracts/ERC20Token.sol
+++ fixtures/contracts/ERC20Token.sol
@@ -1,4 +1,4 @@
-t '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+penzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract ERC20Token is ERC20 {
 	constructor(uint256 initialSupply) ERC20('Gold', 'GLD') {
