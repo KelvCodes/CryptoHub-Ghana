0800a1cfd43a716df1838380847a34ff9cbef692
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-09 13:45:39 -0800

    Update NegativeNumbers.sol

--- fixtures/contracts/NegativeNumbers.sol
+++ fixtures/contracts/NegativeNumbers.sol
@@ -1,4 +1,4 @@
-egativeNumbersIndexed(int256 indexed one, int256 indexed two);
+mbersIndexed(int256 indexed one, int256 indexed two);
 
     /// @notice Event that mixes a positive unsigned integer, a negative number, and a string
     event OtherNegativeNumbers(uint256 positive, int256 negative, string str);
