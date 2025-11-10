1cf4a13e705e4e29e8957c2f88c48e74132abcec
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-10 14:35:08 -0800

    Update ErrorsContract.sol

--- fixtures/contracts/ErrorsContract.sol
+++ fixtures/contracts/ErrorsContract.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GNU
+-License-Identifier: GNU
 pragma solidity ^0.8.13;
 
 /// @title ErrorsContract - Demonstrates custom errors, secure Ether handling, and authorization
