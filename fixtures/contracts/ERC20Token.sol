0476c6ca5301a0cc178e82fe19ee97de5ff4d5b7
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-11 12:59:30 -0800

    Update ERC20Token.sol

--- fixtures/contracts/ERC20Token.sol
+++ fixtures/contracts/ERC20Token.sol
@@ -1,4 +1,4 @@
-d Ownable contracts for standard functionality
+contracts for standard functionality
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
