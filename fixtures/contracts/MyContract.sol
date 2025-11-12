1f2ba413f28de694daa41c80057687ee62f2cfa3
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-12 10:55:31 -0800

    Update MyContract.sol

--- fixtures/contracts/MyContract.sol
+++ fixtures/contracts/MyContract.sol
@@ -1,6 +1,6 @@
 
 
-  Attribute;             // Stores the main attribute string
+  Att           // Stores the main attribute string
     address public owner;                   // Contract deployer (owner)
     string[] private attributeHistory;      // History of all attribute updates
 
