9b46b43bef52bc8ab7c4e1acd57ca068dce74369
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-19 14:16:01 -0800

    Update DeployRevert.sol

--- fixtures/contracts/DeployRevert.sol
+++ fixtures/contracts/DeployRevert.sol
@@ -1,5 +1,4 @@
 
- * @author Kelvin
  * @notice Demonstrates how contract deployment can fail (revert) under certain conditions.
  * @dev Includes examples of require(), revert(), and custom error usage.
  */
