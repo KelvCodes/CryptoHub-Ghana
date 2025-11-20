2884b96bcc48f4096f44e1bc8c02b1e0c2da5568
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-20 10:42:28 -0800

    Update Basic.sol

--- fixtures/contracts/Basic.sol
+++ fixtures/contracts/Basic.sol
@@ -1,7 +1,5 @@
 
 
-// -------------------------------------------------------------
-
 const MNEMONIC = process.env.MNEMONIC;
 const NODE_API_KEY = process.env.INFURA_KEY || process.env.ALCHEMY_KEY;
 const isInfura = !!process.env.INFURA_KEY;
