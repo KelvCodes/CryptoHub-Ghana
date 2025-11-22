3aafa89c5fe9fd5dcb025383e891e795f568ee68
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-22 09:01:02 -0800

    Update ErrorsContract.sol

--- fixtures/contracts/ErrorsContract.sol
+++ fixtures/contracts/ErrorsContract.sol
@@ -1,4 +1,4 @@
-Demonstrates custom errors, secure Ether handling, deposit tracking, partial withdrawals,
+ustom errors, secure Ether handling, deposit tracking, partial withdrawals,
  * ownership control, and emergency pause functionality.
  * @dev Designed for learning safe Ether management and advanced Solidity practices.
  */
