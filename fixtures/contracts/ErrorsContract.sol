7de04946f213fc25224278f04a964503a0fda12b
Kelvin Agyare <onlykelvin06@gmail.com>
2025-11-22 09:10:41 -0800

    Update ErrorsContract.sol

--- fixtures/contracts/ErrorsContract.sol
+++ fixtures/contracts/ErrorsContract.sol
@@ -1,7 +1,5 @@
 
     // =============================
-    // ======== CONSTRUCTOR =======
-    // =============================
 
     /// @notice Sets deployer as initial owner
     constructor() {
