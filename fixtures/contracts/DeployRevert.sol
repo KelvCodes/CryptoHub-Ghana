38105284b46bcac7f35697e27433e1913c316e56
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-06 12:14:11 -0800

    Update DeployRevert.sol

--- fixtures/contracts/DeployRevert.sol
+++ fixtures/contracts/DeployRevert.sol
@@ -1,6 +1,4 @@
-, events, and deployment validation.
- */
-contract DeployRevertPro {
+
     // ===========================================
     // 🔹 STATE VARIABLES
     // ===========================================
