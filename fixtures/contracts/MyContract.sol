443e723f8c785e6987db1fedf97262a900165950
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-07 05:43:40 -0800

    Update MyContract.sol

--- fixtures/contracts/MyContract.sol
+++ fixtures/contracts/MyContract.sol
@@ -1,7 +1,5 @@
 
-    // ======================================
-    modifier onlyOwner() {
-        if (msg.sender != owner) revert Unauthorized(msg.sender);
+    // =======evert Unauthorized(msg.sender);
         _;
     }
 
