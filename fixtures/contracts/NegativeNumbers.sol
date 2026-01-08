a55c05b9cda249344388b0e78627b9959a05f452
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-08 12:33:50 -0800

    Update NegativeNumbers.sol

--- fixtures/contracts/NegativeNumbers.sol
+++ fixtures/contracts/NegativeNumbers.sol
@@ -1,4 +1,4 @@
-t256 positive, int256 negative, string str);
+negative, string str);
     event OtherNegativeNumbersIndexed(uint256 indexed positive, int256 indexed negative, string str);
 
     /// @notice Ownership change event.
