6ad78b56cd24f9687349942ed19a96741256dea8
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-10 08:28:18 -0800

    Update DeployRevert.sol

--- fixtures/contracts/DeployRevert.sol
+++ fixtures/contracts/DeployRevert.sol
@@ -1,9 +1,6 @@
 
     event ExternalCallResult(bool success, bytes data);
 
-    // ======================
-    // =============================================================
-
     modifier onlyDeployer() {
         if (msg.sender != deployer) {
             revert UnauthorizedAccess(msg.sender);
