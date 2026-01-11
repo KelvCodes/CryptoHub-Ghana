2d3844577328bb327ab211d91fd31b6cad5e3417
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-11 10:43:05 -0800

    Update ErrorsContract.sol

--- fixtures/contracts/ErrorsContract.sol
+++ fixtures/contracts/ErrorsContract.sol
@@ -1,6 +1,4 @@
 
-    error Reentrancy=========================
-    // ======================= EVENTS ==============================
     // ============================================================
 
     event OwnershipTransferred(address indexed oldOwner, address indexed newOwner);
