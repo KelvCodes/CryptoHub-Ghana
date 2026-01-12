2c4ecb57a094df11ba08626c8cf6ba11099c8c2d
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-12 00:26:51 -0800

    Update ERC721Token.sol

--- fixtures/contracts/ERC721Token.sol
+++ fixtures/contracts/ERC721Token.sol
@@ -1,5 +1,4 @@
 
-// ============================================================
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
