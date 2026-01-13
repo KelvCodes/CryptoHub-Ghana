fbc0bb9d6b2a86da6a2551742b95814dcedd7704
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-13 05:33:01 -0800

    Update Greeter.sol

--- fixtures/contracts/Greeter.sol
+++ fixtures/contracts/Greeter.sol
@@ -1,4 +1,4 @@
-address indexed oldOwner, address indexed newOwner);
+er, address indexed newOwner);
     event AdminAdded(address indexed admin);
     event AdminRemoved(address indexed admin);
     event ContractPaused(address indexed by);
