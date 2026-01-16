766becf5bc490af94e9bd249ebb45c5cbb6a26ac
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-01-16 13:09:47 -0800

    Update ERC20Token.sol

--- fixtures/contracts/ERC20Token.sol
+++ fixtures/contracts/ERC20Token.sol
@@ -1,5 +1,4 @@
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+lin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
