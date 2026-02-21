6021175bcebd380196ca1c1477dba871d24cd8bc
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-02-21 18:22:50 +0000

    Update Basic.sol

--- fixtures/contracts/Basic.sol
+++ fixtures/contracts/Basic.sol
@@ -1,5 +1,4 @@
 KEY,
-  NETWORK: process.env.NETWORK || "rinkeby",
   OWNER_ADDRESS: process.env.OWNER_ADDRESS,
   NFT_CONTRACT_ADDRESS: process.env.NFT_CONTRACT_ADDRESS,
   OPENSEA_API_KEY: process.env.API_KEY || "",
