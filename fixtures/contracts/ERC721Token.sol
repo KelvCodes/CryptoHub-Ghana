32298d00a11eed4246a5a4ba6240188acbfc40e8
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-03-27 07:20:24 +0000

    Update ERC721Token.sol

--- fixtures/contracts/ERC721Token.sol
+++ fixtures/contracts/ERC721Token.sol
@@ -1,6 +1,4 @@
-=============================================
-
-    event ItemMinted(address indexed to, uint256 indexed tokenId);
+dress indexed to, uint256 indexed tokenId);
     event BatchMinted(address indexed to, uint256 quantity);
     event BaseURIUpdated(string newBaseURI);
     event UnrevealedURIUpdated(string newURI);
