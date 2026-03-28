c453dd4a013c191e94cca39385ef92c96d775aa0
Kelvin Agyare Yeboah <onlykelvin06@gmail.com>
2026-03-28 23:51:41 +0000

    Update ERC721Token.sol

--- fixtures/contracts/ERC721Token.sol
+++ fixtures/contracts/ERC721Token.sol
@@ -7,7 +7,7 @@
     modifier supplyAvailable(uint256 quant
             _tokenIds.current() 
     modifier addressLimit(address user, uint256 
-            "Mint limit exceeded"
+            "Mint limit exce
         );
         _;
     }
